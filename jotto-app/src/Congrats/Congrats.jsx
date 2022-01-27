5c5d6882d9bb4d329285443d31a8159a1f26ba09
Tima Gixe <timagixe@protonmail.com>
2022-01-27 09:54:39 +0200

    implement Congrats component

--- jotto-app/src/Congrats/Congrats.jsx
+++ jotto-app/src/Congrats/Congrats.jsx
@@ -1,1 +1,5 @@
-export const Congrats = () => <div />;
+export const Congrats = ({ success }) => (
+    <div data-test="congrats-component">
+        {success && <span data-test="congrats-message">You guessed the word! Congratulations!</span>}
+    </div>
+);
