38560fc7bd6e18ad207f4b8dfe19ed8a3e15308f
Tima Gixe <timagixe@protonmail.com>
2022-01-27 22:38:53 +0200

    adjust App component view

--- jotto-app/src/App/App.js
+++ jotto-app/src/App/App.js
@@ -1,18 +1,17 @@
-import logo from "../logo.svg";
-import "./App.css";
+import { Congrats } from "../Congrats/Congrats";
+import { GuessedWords } from "../GuessedWords/GuessedWords";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+        <div data-test="app-component">
+            <h1>Guess The Word Game</h1>
+            <Congrats success />
+            <GuessedWords
+                guessedWords={[
+                    { guessedWord: "train", letterMatchCount: 3 },
+                    { guessedWord: "future", letterMatchCount: 6 },
+                ]}
+            />
         </div>
     );
 }
