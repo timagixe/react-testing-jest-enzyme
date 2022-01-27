58fd26054d6cdeb10dc06fdfcee4ba8755e2706d
Tima Gixe <timagixe@protonmail.com>
2022-01-27 10:18:22 +0200

    add PropTypes to Congrats component

--- jotto-app/src/Congrats/Congrats.jsx
+++ jotto-app/src/Congrats/Congrats.jsx
@@ -1,5 +1,11 @@
+import PropTypes from "prop-types";
+
 export const Congrats = ({ success }) => (
     <div data-test="congrats-component">
         {success && <span data-test="congrats-message">You guessed the word! Congratulations!</span>}
     </div>
 );
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired,
+};
