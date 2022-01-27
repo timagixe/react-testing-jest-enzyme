ac9761309e3bc8843453f8e77ca47b63cfbc19c5
Tima Gixe <timagixe@protonmail.com>
2022-01-27 22:38:35 +0200

    implement App tests

--- jotto-app/src/App/App.test.js
+++ jotto-app/src/App/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow } from "enzyme";
+import { findByDataTestAttribute } from "../utils";
+import App from "./App";
+import { Congrats } from "../Congrats/Congrats";
+import { GuessedWords } from "../GuessedWords/GuessedWords";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const setup = (props = {}) => shallow(<App {...props} />);
+
+describe("App component", () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = setup(<App />);
+    });
+
+    test("renders", () => {
+        // eslint-disable-next-line testing-library/await-async-query
+        const appComponent = findByDataTestAttribute(wrapper, "app-component");
+        expect(appComponent).toHaveLength(1);
+    });
+
+    test("renders Congrats component", () => {
+        const congratsComponent = wrapper.find(Congrats);
+        expect(congratsComponent).toHaveLength(1);
+    });
+
+    test("renders GuessedWords component", () => {
+        const guessedWordsComponent = wrapper.find(GuessedWords);
+        expect(guessedWordsComponent).toHaveLength(1);
+    });
 });
