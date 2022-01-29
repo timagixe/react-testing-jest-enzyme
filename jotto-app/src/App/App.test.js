7c2021aca70a1cca62cab2a1af40cb131f1002d6
Tima Gixe <timagixe@protonmail.com>
2022-01-29 12:27:33 +0200

    update App tests

--- jotto-app/src/App/App.test.js
+++ jotto-app/src/App/App.test.js
@@ -1,31 +1,153 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { findByDataTestAttribute } from "../utils";
 import App from "./App";
-import { Congrats } from "../Congrats/Congrats";
-import { GuessedWords } from "../GuessedWords/GuessedWords";
 
-const setup = (props = {}) => shallow(<App {...props} />);
+const setup = (state = {}) => {
+    const wrapper = mount(<App />);
+
+    // eslint-disable-next-line testing-library/await-async-query
+    const inputNode = findByDataTestAttribute(wrapper, "input-node");
+    inputNode.simulate("change", { target: { value: "station" } });
+
+    // eslint-disable-next-line testing-library/await-async-query
+    const submitButton = findByDataTestAttribute(wrapper, "submit-button");
+    submitButton.simulate("click", { preventDefault: () => {} });
+
+    return wrapper;
+};
 
 describe("App component", () => {
-    let wrapper;
+    describe("with no guessed words", () => {
+        let wrapper;
 
-    beforeEach(() => {
-        wrapper = setup(<App />);
+        beforeEach(() => {
+            wrapper = setup({
+                success: false,
+                guessedWords: [],
+                secretWord: "daylight",
+            });
+        });
+
+        test("renders guess-words-instructions", () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const guessWordInstructions = findByDataTestAttribute(wrapper, "guess-words-instructions");
+            expect(guessWordInstructions).toHaveLength(1);
+        });
+
+        test("does not render guessed-words-section", () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const guessedWordsSection = findByDataTestAttribute(wrapper, "guessed-words-section");
+            expect(guessedWordsSection).toHaveLength(0);
+        });
     });
 
-    test("renders", () => {
-        // eslint-disable-next-line testing-library/await-async-query
-        const appComponent = findByDataTestAttribute(wrapper, "app-component");
-        expect(appComponent).toHaveLength(1);
+    describe("with some guessed words", () => {
+        let wrapper;
+        const state = {
+            success: false,
+            guessedWords: [
+                { guessedWord: "train", letterMatchCount: 2 },
+                { guessedWord: "future", letterMatchCount: 1 },
+            ],
+            secretWord: "daylight",
+        };
+
+        beforeEach(() => {
+            wrapper = setup(state);
+        });
+
+        test("does not render congrats-message", () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const congratsMessage = findByDataTestAttribute(wrapper, "congrats-message");
+            expect(congratsMessage).toHaveLength(0);
+        });
+
+        test.todo(
+            "does not render guess-words-instructions" /* , () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const guessWordsInstructions = findByDataTestAttribute(wrapper, "guess-words-instructions");
+            expect(guessWordsInstructions).toHaveLength(0);
+        } */
+        );
+
+        test.todo(
+            "renders guessed-words-section" /* , () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const guessedWordsSection = findByDataTestAttribute(wrapper, "guessed-words-section");
+            expect(guessedWordsSection).toHaveLength(1);
+        } */
+        );
+
+        test.todo(
+            "renders as many guessed-word elements as passed in guessedWords plus one" /* , () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const guessedWord = findByDataTestAttribute(wrapper, "guessed-word");
+            expect(guessedWord).toHaveLength(state.guessedWords.length + 1);
+        }*/
+        );
     });
 
-    test("renders Congrats component", () => {
-        const congratsComponent = wrapper.find(Congrats);
-        expect(congratsComponent).toHaveLength(1);
-    });
+    describe("with guessed secret word", () => {
+        let wrapper;
+        const state = {
+            success: false,
+            guessedWords: [
+                { guessedWord: "train", letterMatchCount: 2 },
+                { guessedWord: "future", letterMatchCount: 1 },
+            ],
+            secretWord: "daylight",
+        };
 
-    test("renders GuessedWords component", () => {
-        const guessedWordsComponent = wrapper.find(GuessedWords);
-        expect(guessedWordsComponent).toHaveLength(1);
+        beforeEach(() => {
+            wrapper = setup(state);
+        });
+
+        test.todo(
+            "renders congrats-message" /* , () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const congratsMessage = findByDataTestAttribute(wrapper, "congrats-message");
+            expect(congratsMessage).toHaveLength(1);
+        } */
+        );
+
+        test.todo(
+            "does not render input-node" /* , () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const inputNode = findByDataTestAttribute(wrapper, "input-node");
+            expect(inputNode).toHaveLength(0);
+        } */
+        );
+
+        test.todo(
+            "does not render submit-button" /* , () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const submitButton = findByDataTestAttribute(wrapper, "submit-button");
+            expect(submitButton).toHaveLength(0);
+        } */
+        );
+
+        test.todo(
+            "does not render guess-words-instructions" /* , () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const guessWordsInstructions = findByDataTestAttribute(wrapper, "guess-words-instructions");
+            expect(guessWordsInstructions).toHaveLength(0);
+        } */
+        );
+
+        test.todo(
+            "renders guessed-words-section" /* , () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const guessedWordsSection = findByDataTestAttribute(wrapper, "guessed-words-section");
+            expect(guessedWordsSection).toHaveLength(1);
+        } */
+        );
+
+        test.todo(
+            "renders as many guessed-word as passed in guessedWords plus one" /* , () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const guessedWord = findByDataTestAttribute(wrapper, "guessed-word");
+            expect(guessedWord).toHaveLength(state.guessedWords + 1);
+        } */
+        );
     });
 });
