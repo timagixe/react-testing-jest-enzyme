46d9f155d89430411517bfffdb9e5aa948557976
Tima Gixe <timagixe@protonmail.com>
2022-01-29 12:27:39 +0200

    update App component

--- jotto-app/src/App/App.js
+++ jotto-app/src/App/App.js
@@ -1,17 +1,19 @@
+import React from "react";
 import { Congrats } from "../Congrats/Congrats";
 import { GuessedWords } from "../GuessedWords/GuessedWords";
+import { SecretWordInput } from "../SecretWordInput/SecretWordInput";
 
 function App() {
+    const [secretWord] = React.useState("daylight");
+    const [success] = React.useState(false);
+    const [guessedWords] = React.useState([]);
+
     return (
         <div data-test="app-component" className="container">
             <h1 className="display-4 d-flex justify-content-center">Guess The Word Game</h1>
-            <Congrats success />
-            <GuessedWords
-                guessedWords={[
-                    { guessedWord: "train", letterMatchCount: 3 },
-                    { guessedWord: "future", letterMatchCount: 6 },
-                ]}
-            />
+            <Congrats success={success} />
+            <SecretWordInput secretWord={secretWord} success={success} />
+            <GuessedWords guessedWords={guessedWords} />
         </div>
     );
 }
