bd889145fc036400004124a8a21fe5285c289ebd
Tima Gixe <timagixe@protonmail.com>
2022-02-21 10:42:09 +0200

    re-write App to use useReducer

--- jotto-app-context/src/App.js
+++ jotto-app-context/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useReducer } from "react";
 import "./App.css";
 
 import Congrats from "./Congrats";
@@ -6,20 +6,33 @@ import GuessedWords from "./GuessedWords";
 import Input from "./Input";
 import { getSecretWord } from "./actions";
 
+const reducerFunction = (state, action) => {
+    switch (action.type) {
+        case "setSecretWord":
+            return { ...state, secretWord: action.payload };
+        default:
+            throw Error(`Unknown action type ${action.type}`);
+    }
+};
+
 function App() {
-    const [secretWord, setSecretWord] = useState("party");
+    const [state, dispatch] = useReducer(reducerFunction, { secretWord: "" });
     const success = false;
     const guessedWords = [];
 
+    const setSecretWord = useCallback((secretWord) => {
+        dispatch({ type: "setSecretWord", payload: secretWord });
+    }, []);
+
     useEffect(() => {
         getSecretWord(setSecretWord);
-    }, []);
+    }, [setSecretWord]);
 
     return (
         <div data-test="component-app" className="container">
             <h1>Jotto</h1>
             <Congrats success={success} />
-            <Input success={success} secretWord={secretWord} />
+            <Input success={success} secretWord={state.secretWord} />
             <GuessedWords guessedWords={guessedWords} />
         </div>
     );
