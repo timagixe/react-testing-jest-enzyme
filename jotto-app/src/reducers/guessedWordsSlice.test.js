dbb21bdf56d799386596da43200f24b985621916
Tima Gixe <timagixe@protonmail.com>
2022-02-21 07:30:38 +0200

    update guessedWordsSlice tests

--- jotto-app/src/reducers/guessedWordsSlice.test.js
+++ jotto-app/src/reducers/guessedWordsSlice.test.js
@@ -1,3 +1,18 @@
+import { getGuessedWordsReducerInitialState, guessedWordsActions, guessedWordsReducer } from "./guessedWordsSlice";
+
+const { updateGuessedWords } = guessedWordsActions;
 describe("guessedWordsSlice", () => {
-    test.todo("guessedWordsSlice tests");
+    test("initial state is empty array", () => {
+        const initialState = getGuessedWordsReducerInitialState();
+        expect(initialState).toEqual([]);
+    });
+
+    test("initial state is not empty after guess", () => {
+        const initialState = getGuessedWordsReducerInitialState();
+        const newState = guessedWordsReducer(
+            initialState,
+            updateGuessedWords({ guessedWord: "future", letterMatchCount: 1 })
+        );
+        expect(newState).not.toHaveLength(0);
+    });
 });
