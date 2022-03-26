d1accb019419722a59339f15c3f0353f3a44624b
Tima Gixe <timagixe@protonmail.com>
2022-03-26 14:13:51 +0200

    implement language tests for input

--- jotto-app-context/src/App.test.js
+++ jotto-app-context/src/App.test.js
@@ -3,6 +3,7 @@ import { mount } from "enzyme";
 import { findByTestAttr } from "../test/testUtils";
 import App from "./App";
 import { getSecretWord as mockGetSecretWord } from "./actions";
+import { DEFAULT_LANGUAGE } from "./contexts/languageContext";
 
 // activate global mock to make sure getSecretWord doesn't make network call
 jest.mock("./actions");
@@ -12,56 +13,58 @@ jest.mock("./actions");
  * @returns {Wrapper}
  */
 const setup = () => {
-    // use mount, because useEffect not called on `shallow`
-    // https://github.com/airbnb/enzyme/issues/2086
-    return mount(<App />);
+  // use mount, because useEffect not called on `shallow`
+  // https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App />);
 };
 
 describe.each([
-    [null, true, false],
-    ["party", false, true],
+  [null, true, false],
+  ["party", false, true],
 ])("renders with secretWord as %s", (secretWord, isLoading, isAppLoaded) => {
-    let originalUserReducer;
-    let wrapper;
+  let originalUserReducer;
+  let wrapper;
 
-    beforeEach(() => {
-        originalUserReducer = React.useReducer;
-        React.useReducer = jest.fn().mockReturnValue([{ secretWord }, jest.fn()]);
-        wrapper = setup();
-    });
+  beforeEach(() => {
+    originalUserReducer = React.useReducer;
+    React.useReducer = jest
+      .fn()
+      .mockReturnValue([{ secretWord, language: DEFAULT_LANGUAGE }, jest.fn()]);
+    wrapper = setup();
+  });
 
-    afterEach(() => {
-        React.useReducer = originalUserReducer;
-    });
+  afterEach(() => {
+    React.useReducer = originalUserReducer;
+  });
 
-    test(`renders loading spinner: ${isLoading}`, () => {
-        const loadingSpinner = findByTestAttr(wrapper, "spinner");
-        expect(loadingSpinner.exists()).toBe(isLoading);
-    });
+  test(`renders loading spinner: ${isLoading}`, () => {
+    const loadingSpinner = findByTestAttr(wrapper, "spinner");
+    expect(loadingSpinner.exists()).toBe(isLoading);
+  });
 
-    test(`renders app component: ${isAppLoaded}`, () => {
-        const loadingSpinner = findByTestAttr(wrapper, "component-app");
-        expect(loadingSpinner.exists()).toBe(isAppLoaded);
-    });
+  test(`renders app component: ${isAppLoaded}`, () => {
+    const loadingSpinner = findByTestAttr(wrapper, "component-app");
+    expect(loadingSpinner.exists()).toBe(isAppLoaded);
+  });
 });
 
 describe("get secret word", () => {
-    beforeEach(() => {
-        // clear the mock calls from previous tests
-        mockGetSecretWord.mockClear();
-    });
-    test("getSecretWord on app mount", () => {
-        const wrapper = setup();
-        expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
-    });
-    test("getSecretWord does not run on app update", () => {
-        const wrapper = setup();
-        mockGetSecretWord.mockClear();
+  beforeEach(() => {
+    // clear the mock calls from previous tests
+    mockGetSecretWord.mockClear();
+  });
+  test("getSecretWord on app mount", () => {
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+  test("getSecretWord does not run on app update", () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
 
-        // using setProps because wrapper.update() doesn't trigger useEffect
-        // https://github.com/enzymejs/enzyme/issues/2254
-        wrapper.setProps();
+    // using setProps because wrapper.update() doesn't trigger useEffect
+    // https://github.com/enzymejs/enzyme/issues/2254
+    wrapper.setProps();
 
-        expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
-    });
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
 });
--- jotto-app-context/src/Input.test.js
+++ jotto-app-context/src/Input.test.js
@@ -1,64 +1,101 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { findByTestAttr, checkProps } from '../test/testUtils';
+import React from "react";
+import { mount } from "enzyme";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 
-import Input from './Input';
+import {
+  DEFAULT_LANGUAGE,
+  LanguageContextProvider,
+} from "./contexts/languageContext";
+import Input from "./Input";
 
-// mock entire module for destructuring useState on import //////
-// const mockSetCurrentGuess = jest.fn();
-// jest.mock('react', () => ({
-//   ...jest.requireActual('react'),
-//   useState: (initialState) => [initialState, mockSetCurrentGuess]
-// }))
+/**
+ * Factory function to create a ShallowWrapper for the Congrats component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @returns {ReactWrapper}
+ */
+const setup = ({
+  success = false,
+  secretWord = "party",
+  language = DEFAULT_LANGUAGE,
+} = {}) => {
+  return mount(
+    <LanguageContextProvider value={language}>
+      <Input success={success} secretWord={secretWord} />
+    </LanguageContextProvider>,
+  );
+};
 
-const setup = (success=false, secretWord='party') => {
-  return shallow(<Input success={success} secretWord={secretWord} />);
-}
-
-describe('render', () => {
-  describe('success is false', () => {
+describe("render", () => {
+  describe("success is false", () => {
     let wrapper;
     beforeEach(() => {
-      wrapper = setup(false);
-    })
-    test('Input renders without error', () => {
-      const inputComponent = findByTestAttr(wrapper, 'component-input');
+      wrapper = setup({ success: false });
+    });
+    test("Input renders without error", () => {
+      const inputComponent = findByTestAttr(wrapper, "component-input");
       expect(inputComponent.length).toBe(1);
     });
-    test('input box displays', () => {
-      const inputBox = findByTestAttr(wrapper, 'input-box');
+    test("input box displays", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
       expect(inputBox.exists()).toBe(true);
     });
-    test('submit button displays', () => {
-      const submitButton = findByTestAttr(wrapper, 'submit-button');
+    test("submit button displays", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
       expect(submitButton.exists()).toBe(true);
     });
   });
-  describe('success is true', () => {
+  describe("success is true", () => {
     let wrapper;
     beforeEach(() => {
-      wrapper = setup(true);
-    })
-    test('Input renders without error', () => {
-      const inputComponent = findByTestAttr(wrapper, 'component-input');
+      wrapper = setup({ success: true });
+    });
+    test("Input renders without error", () => {
+      const inputComponent = findByTestAttr(wrapper, "component-input");
       expect(inputComponent.length).toBe(1);
     });
-    test('input box does not display', () => {
-      const inputBox = findByTestAttr(wrapper, 'input-box');
+    test("input box does not display", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
       expect(inputBox.exists()).toBe(false);
     });
-    test('submit button does not display', () => {
-      const submitButton = findByTestAttr(wrapper, 'submit-button');
+    test("submit button does not display", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
       expect(submitButton.exists()).toBe(false);
     });
   });
 });
 
-test('does not throw warning with expected props', () => {
-  checkProps(Input, { secretWord: 'party' });
-})
+describe("language picker", () => {
+  test.each([
+    ["placeholder", "english", { expected: "enter guess", language: "en" }],
+    ["placeholder", "emoji", { expected: "⌨️🤔", language: "emoji" }],
+    [
+      "placeholder",
+      "undefined",
+      { expected: "enter guess", language: undefined },
+    ],
+  ])("renders [%s] text in [%s]", (_, __, { expected, language }) => {
+    const wrapper = setup({ language });
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    expect(inputBox.prop("placeholder")).toBe(expected);
+  });
 
-describe('state controlled input field', () => {
+  test.each([
+    ["button", "english", { expected: "Submit", language: "en" }],
+    ["button", "emoji", { expected: "🚀", language: "emoji" }],
+    ["button", "undefined", { expected: "Submit", language: undefined }],
+  ])("renders [%s] text in [%s]", (_, __, { expected, language }) => {
+    const wrapper = setup({ language });
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    expect(submitButton.text()).toBe(expected);
+  });
+});
+
+test("does not throw warning with expected props", () => {
+  checkProps(Input, { secretWord: "party" });
+});
+
+describe("state controlled input field", () => {
   let mockSetCurrentGuess = jest.fn();
   let wrapper;
   let originalUseState;
@@ -72,18 +109,18 @@ describe('state controlled input field', () => {
   afterEach(() => {
     React.useState = originalUseState;
   });
-  test('state updates with value of input box upon change', () => {
-    const inputBox = findByTestAttr(wrapper, 'input-box');
-    const mockEvent = { target: { value: 'train' } };
+  test("state updates with value of input box upon change", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    const mockEvent = { target: { value: "train" } };
 
     inputBox.simulate("change", mockEvent);
-    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
   });
-  test('field is cleared upon submit button click', () => {
-    const inputBox = findByTestAttr(wrapper, 'input-box');
-    const mockEvent = { target: { value: 'train' } };
+  test("field is cleared upon submit button click", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    const mockEvent = { target: { value: "train" } };
 
     inputBox.simulate("change", mockEvent);
-    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
   });
-})
+});
